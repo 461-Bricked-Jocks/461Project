8cf602d4de8c37f968c1719f0dadd82245c31d70
Richard Pham <richardphamdq@utexas.edu>
2023-10-04 15:18:11 -0500

    this should work?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 // import logo from './logo.svg';
 import './App.css';
-import Login from './components/login';
+import Login from './components/login'
+import Projects from './pages/projects.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login />
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />}>
+                    {/*<Route path="projects" element={<Projects />} />*/}
+                    {/*/!*{ <Route path="*" element={<NoPage />} />}*!/*/}
+                </Route>
+                <Route path="/projects" element={<Projects />}>
+                    {/*<Route path="projects" element={<Projects />} />*/}
+                    {/*/!*{ <Route path="*" element={<NoPage />} />}*!/*/}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/projects.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "../styles/projects.css";
+
+export default function Projects() {
+    return (
+        <div id = "back">
+
+            <div id = "login">
+                <h1>Create new Project</h1>
+                <label>Name   </label>
+                <input type='text' name='user'></input>
+                <br />
+                <label>Description   </label>
+                <input type='text' name='user'></input>
+                <br />
+                <label>Project ID   </label>
+                <input type='text' name='user'></input>
+                <br />
+                <button>Enter</button>
+                <br></br>
+                <h1>Use Existing Project</h1>
+                <label>Project ID   </label>
+                <input type='text' name='user'></input>
+                <br />
+                <button>Enter</button>
+                <br></br>
+            </div>
+
+        </div>
+    )
+}
