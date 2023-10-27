cda11b064328565494b7a0ac5fc4378011c37f22
Kiet Le <Kietle24@utexas.edu>
2023-10-26 19:05:36 -0500

    Renamed API.py to app.py and other fucntion names

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     try:
-        if request.method == 'POST':    # For debugging purposes  
+        if request.method == 'POST':    # For Testing Purposes  
             data = request.get_json()
             response = Mongodb_Users.existingAccount(data["Username"], data["Password"])
             return response
@@ -20,7 +20,7 @@ def login():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
-@app.route('/create', methods=["POST"])
+@app.route('/Create-User', methods=["POST"])
 def create():
     try:
         data = request.get_json()
@@ -29,17 +29,9 @@ def create():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
    
-@app.route('/Projects', methods=["POST"])
-def create():
-    try:
-        data = request.get_json()
-        response = Mongodb_Hardware.collection_hardware() # Work In Progress
-        return response
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
     
-@app.route('/New-Project', methods=["POST"])
-def create():
+@app.route('/New-Project', methods=["POST"]) # TODO
+def createProject():
     try:
         data = request.get_json()
         
@@ -49,8 +41,8 @@ def create():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-@app.route('/Checkout', methods=["POST"])
-def create():
+@app.route('/Checkout', methods=["POST"]) # TODO
+def checkout():
     try:
         data = request.get_json()
 
@@ -60,8 +52,8 @@ def create():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-@app.route('/Checkin', methods=["POST"])
-def create():
+@app.route('/Checkin', methods=["POST"]) # TODO
+def checkin():
     try:
         data = request.get_json()
 
