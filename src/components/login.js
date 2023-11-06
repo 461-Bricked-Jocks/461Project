33f5cc0457e53200509de9f8f76d956724160203
Jesus61302 <jesushernandez61302@gmail.com>
2023-11-05 20:15:03 -0600

    added redirecting functionallity

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./login.css";
-import { BrowserRouter as Routes, Route, useNavigate, Link } from "react-router-dom";
+import { BrowserRouter as Routes, Route, useNavigate, Link, Navigate } from "react-router-dom";
 import Projects from '../pages/projects';
 // import { render } from '@testing-library/react';
 
@@ -50,14 +50,19 @@ class Login extends React.Component {
       // .then(data => this.handler(data["Access"]))
       .then((success) => {
         var Access = success['Access']
+        var EncryptedUser = success['Username']
+        var EncryptedPass = success['Password']
         console.log(Access)
         this.setState({
-          success : Access
+          success : Access,
+          // user : EncryptedUser,
+          // pass : EncryptedPass
         })
       })
       setTimeout(() => {
         if(this.state.success === true){
           window.alert("successfully signed in")
+          window.location.replace(`/Projects?Username=${this.state.user}&Password=${this.state.pass}`)
         }else{
           alert("Incorrect username or password")
         }
@@ -67,11 +72,12 @@ class Login extends React.Component {
 
   }
   test(){
-    return <Link to={"/projects"}>herllo</Link>
+    window.location.replace("/Projects?Username=abc123&Password=pass123");
   }
 
   render(){
     return (
+      
       <div id = "back">
   
         <div id = "login">
@@ -83,7 +89,7 @@ class Login extends React.Component {
           <input id="password" type='password' name='user' value={this.pass} onChange={this.passwordHandler}></input>
           <br/><br/>
           <button onClick={this.login} id='loginBtn'>Login</button>
-          <button onClick={this.test}>test</button>
+          {/* <button onClick={this.test}>test</button> */}
           <br/><br/>
           <Link to={"/projects"}>Forgot Password</Link>
           <br/><br/>
