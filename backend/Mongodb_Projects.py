bdd7255ccff2996daee628fb0bc2d4bef51ea125
Jesus61302 <jesushernandez61302@gmail.com>
2023-11-08 19:28:02 -0600

    fixed bugs int the creat-project page

--- backend/Mongodb_Projects.py
+++ backend/Mongodb_Projects.py
@@ -60,8 +60,10 @@ def join_project(username, projectName):
         response = {"Access": False }
         return response
     if (does_project_nameexist(projectName)):
+        print("this runs")
         data = collection_projects.find_one({"Name": projectName})["_id"]
         query = {"_id": data}
+        print(query)
         update = {
             "$push": {
                 "Users": username
@@ -78,7 +80,9 @@ def join_project(username, projectName):
                 "projects": projectName
             }
         }
-        collection_projects.update_one(query, update)
+        collection_users.update_one(query, update)
+        response = {"Access": True }
+        return response
     else:
         response = {"Access": False }
         return response
